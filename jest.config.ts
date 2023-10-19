c7dc38f00d5a5fb068bfd099f339420ded25d4fa
valen <valenjaramillo1025@hotmail.com>
2023-10-19 09:14:39 -0500

    Add jest set up

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    // process `*.tsx` files with `ts-jest`
+  },
+  rootDir: "src",
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+};
+
