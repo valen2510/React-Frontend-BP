0a49ef3707dbb41a8a2fda5c1f414c83150e568c
valen <valenjaramillo1025@hotmail.com>
2023-10-21 12:05:50 -0500

    Add libraries of axios and react-hooks, and adjust config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+      features: "/src/features",
+      components: "/src/components",
+      assets: "/src/assets",
+      routes: "/src/routes",
+      services: "/src/services",
+      utils: "/src/utils",
+    },
+  },
+});
